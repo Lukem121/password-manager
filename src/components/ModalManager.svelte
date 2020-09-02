c55ce268e26a4ed9ab4ce0c8036fdda96c2ef4f2
Luke <lukeask@hotmail.co.uk>
2020-09-02 15:12:18 +0100

    Added import modal

--- src/components/ModalManager.svelte
+++ src/components/ModalManager.svelte
@@ -1,11 +1,14 @@
 <script>
     import { modalStore } from '../DataStores/ModalStateStore.js';
     import NewVault from './UserModals/NewVault.svelte';
+    import ImportVault from './UserModals/ImportVault.svelte';
 </script>
 
 <div>
     {#if $modalStore == "new"}
         <NewVault />
+    {:else if $modalStore == "import"}
+        <ImportVault />
     {/if}
 </div>
 
---
+++ src/components/UserModals/ImportVault.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { modalStore } from '../../DataStores/ModalStateStore.js';
+
+    import Modal from '../ReusableComponents/Modal.svelte';
+    import Card from '../ReusableComponents/Card.svelte';
+    import Button from '../ReusableComponents/Button.svelte';
+
+    let importString;
+    let errorMessage; 
+
+    const handleClick = () => {
+
+    }
+</script>
+
+<Modal>
+    <Card closeButton={true} errorMessage={errorMessage} on:closeClick>
+        <span slot="title">
+            Already have a vault? 
+        </span>
+
+        <span slot="content">
+            <textarea bind:value={importString} rows="15" cols="80" placeholder="Paste import string here..."></textarea>
+        </span>
+
+        <span slot="action-btn">
+            <Button on:click={handleClick} icon={"import"}>Import</Button>
+        </span>
+    </Card>
+</Modal>
+
+<style>
+    textarea {
+        width: 100%;
+    }
+</style>
